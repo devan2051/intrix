d4487b9dd51c29536399f7a458b3483938692eae
devan2051 <81530629+devan2051@users.noreply.github.com>
2026-08-27 00:48:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -196,6 +196,7 @@ if result is not None:
     )
 
     st.markdown("#### 🧭  Where do you want to go?")
+    st.markdown("Destination")")
     dest_name = st.selectbox("Destination", locations_df["name"].tolist(), width=420)
     dest_row = locations_df[locations_df["name"] == dest_name].iloc[0]
     dest_distance = distance_to_destination(result["x_est"], result["y_est"], dest_row["x"], dest_row["y"])
